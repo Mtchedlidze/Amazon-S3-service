12da8f61bd3eb2112a49023ba191cd620165fdb3
DESKTOP-Q5TK24I\User <emtchedlidze@unisens.ge>
2022-01-14 18:09:11 +0400

    sa

--- app/aws/controllers/open.js
+++ app/aws/controllers/open.js
@@ -1,37 +1,13 @@
+/* eslint-disable comma-dangle */
 import s3 from '../awsconfig.js'
 
 export default async function open(file) {
-  // return new Promise((resolve, reject) => {
-  //   const res = s3
-  //     .getObject({
-  //       Bucket: 'filemanagment',
-  //       Key: file,
-  //     })
-  //     .createReadStream()
-  //     .on('error', (error) => {
-  //       const err = {
-  //         message: error.message,
-  //         status: error.statusCode,
-  //       }
-  //       reject(err)
-  //     })
-  //     .once('data', resolve)
-  // })
-
-  try {
-    const res = await s3
-      .getObject({
-        Bucket: 'filemanagment',
-        Key: file,
-      })
-      .promise()
-
-    return res.Body.data.createReadStream()
-  } catch (error) {
-    const err = {
-      message: error.message,
-      status: error.statusCode,
-    }
-    return err
-  }
+  return new Promise((resolve, reject) => {
+    s3.getObject({ Bucket: 'filemanagment', Key: file }, (error, data) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(data)
+    }).createReadStream()
+  })
 }
